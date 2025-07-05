1a205baaceb3ea7620250734abb7bd536cc846db
Aryan Pandita <aryanpandita2015@gmail.com>
2025-07-05 12:37:35 +0530

    Update Controls.jsx

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,31 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 function Controls({ size, winLen, updateSize, updateWinLen, onReset }) {
+  const handleSizeChange = (delta) => {
+    const newSize = size + delta;
+    if (newSize >= 3 && newSize <= 10) {
+      updateSize({ target: { value: newSize } });
+    }
+  };
+
+  const handleWinLenChange = (delta) => {
+    const newWinLen = winLen + delta;
+    if (newWinLen >= 3 && newWinLen <= size) {
+      updateWinLen({ target: { value: newWinLen } });
+    }
+  };
+
   return (
     <div className="controls">
-      <label>
-        Board size N:
-        <input
-          type="number"
-          min="3"
-          max="10"
-          value={size}
-          onChange={updateSize}
-        />
-      </label>
-      <label>
-        Win length M:
-        <input
-          type="number"
-          min="3"
-          max={size}
-          value={winLen}
-          onChange={updateWinLen}
-        />
-      </label>
+      <div className="input-group">
+        <label>Board size N:</label>
+        <div className="custom-input">
+          <button onClick={() => handleSizeChange(-1)}>-</button>
+          <input type="number" value={size} readOnly />
+          <button onClick={() => handleSizeChange(1)}>+</button>
+        </div>
+      </div>
+
+      <div className="input-group">
+        <label>Win length M:</label>
+        <div className="custom-input">
+          <button onClick={() => handleWinLenChange(-1)}>-</button>
+          <input type="number" value={winLen} readOnly />
+          <button onClick={() => handleWinLenChange(1)}>+</button>
+        </div>
+      </div>
+
       <button onClick={onReset}>Reset</button>
     </div>
-  )
+  );
 }
 
-export default Controls
+export default Controls;
